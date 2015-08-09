93385660b9aad432beec59d222b3b6736aa6dcfc
virtualmarc <admin@vmtek.de>
2015-08-09 17:14:07 +0200

    Prompt to run .exe and .msi

---
+++ Win10Config/Configuration/AskRunExeMsi.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using Win10Config.Interfaces;
+using Win10Config.Utils;
+
+namespace Win10Config.Configuration
+{
+    class AskRunExeMsi : IConfiguration
+    {
+        private Boolean bRun = false;
+        private String sError;
+        private String sRegistryKey = @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Associations";
+        private String sRegistryValue = "LowRiskFileTypes";
+
+        public void changeValue()
+        {
+            bRun = Input.inputBoolean(getDisplayName(), "Disable Prompt to run .exe and .msi?", bRun);
+        }
+
+        public string getDisplayName()
+        {
+            return "Disable Prompt to run .exe and .msi";
+        }
+
+        public string getDisplayValue()
+        {
+            return bRun ? "Yes" : "No";
+        }
+
+        public bool run()
+        {
+            try
+            {
+                if (bRun)
+                {
+                    Registry.SetValue(sRegistryKey, sRegistryValue, ".exe;.msi;", RegistryValueKind.String);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                sError = ex.Message;
+                return false;
+            }
+        }
+
+        public string getErrorText()
+        {
+            return sError;
+        }
+    }
+}
