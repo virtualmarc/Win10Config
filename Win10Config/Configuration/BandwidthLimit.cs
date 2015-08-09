ed84fd1c0cbf4f721cee71d3e702dc1df362c7ec
virtualmarc <admin@vmtek.de>
2015-08-09 17:11:07 +0200

    Bandwidth Limit

---
+++ Win10Config/Configuration/BandwidthLimit.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using Win10Config.Interfaces;
+using Win10Config.Utils;
+
+namespace Win10Config.Configuration
+{
+    class BandwidthLimit : IConfiguration
+    {
+        private Boolean bRun = false;
+        private String sError;
+        private String sRegistryKey = @"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Psched";
+        private String sRegistryValue = "NonBestEffortLimit";
+
+        public void changeValue()
+        {
+            bRun = Input.inputBoolean(getDisplayName(), "Disable Bandwidth Limit?", bRun);
+        }
+
+        public string getDisplayName()
+        {
+            return "Disable Bandwidth Limit";
+        }
+
+        public string getDisplayValue()
+        {
+            return bRun ? "Yes" : "No";
+        }
+
+        public bool run()
+        {
+            try
+            {
+                if (bRun)
+                {
+                    Registry.SetValue(sRegistryKey, sRegistryValue, 0x0, RegistryValueKind.DWord);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                sError = ex.Message;
+                return false;
+            }
+        }
+
+        public string getErrorText()
+        {
+            return sError;
+        }
+    }
+}
--- Win10Config/frmMain.cs
+++ Win10Config/frmMain.cs
@@ -34,6 +34,7 @@ namespace Win10Config
             lstOptions.Items.Add(new W10ListViewItem(new LockScreen()));
             lstOptions.Items.Add(new W10ListViewItem(new Cortana()));
             lstOptions.Items.Add(new W10ListViewItem(new WebSearch()));
+            lstOptions.Items.Add(new W10ListViewItem(new BandwidthLimit()));
             lstOptions.Items.Add(new W10ListViewItem(new OneDrive()));
             lstOptions.Items.Add(new W10ListViewItem(new Uninstall3DBuilder()));
             lstOptions.Items.Add(new W10ListViewItem(new UninstallBingFinance()));
