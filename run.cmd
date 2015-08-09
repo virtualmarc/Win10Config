abc568b145994116d6ae531a725596f1e1b6447e
virtualmarc <admin@vmtek.de>
2015-08-09 10:35:21 +0200

    Prepare for Rewrite

--- run.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /D %~dp0
-powershell "Set-ExecutionPolicy Unrestricted -Force"
-xcopy /E /Y /I PSWindowsUpdate %WINDIR%\System32\WindowsPowerShell\v1.0\Modules\PSWindowsUpdate
-powershell .\config.ps1
-pause
