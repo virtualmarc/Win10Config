9d887f2ebab380582346540f8930be2f1f4353c9
virtualmarc <admin@vmtek.de>
2015-08-10 19:44:06 +0200

    #7 Fixed Object Creation

--- Win10Config/Configuration/BlockMSAccount.cs
+++ Win10Config/Configuration/BlockMSAccount.cs
@@ -19,7 +19,12 @@ namespace Win10Config.Configuration
 
         public BlockMSAccount()
         {
-            bRun = (int)Registry.GetValue(sRegistryKey, sRegistryValue, 0x1) == 0x0;
+            try {
+                bRun = (int)Registry.GetValue(sRegistryKey, sRegistryValue, 0x1) == 0x0;
+            } catch (Exception)
+            {
+                bRun = false;
+            }
         }
 
         public void changeValue()
